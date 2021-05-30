813c64c660527e37abb35764dc825db2302bd82d
reooo <leohattori@icloud.com>
2021-05-30 17:02:37 +0900

    first commit

---
+++ OpenGL_Introduction/OpenGL_Introduction/OpenGL_Introduction.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+
+void disp(void) {
+	glClearColor(1, 1, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowPosition(100, 50);
+	glutInitWindowSize(200, 100);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+
+	glutCreateWindow("Kitty on your lap");
+	glutDisplayFunc(disp);
+	glutMainLoop();
+	return 0;
+}
---
+++ OpenGL_Introduction/OpenGL_Introduction/pch.h
@@ -0,0 +1,7 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <windows.h>
+#include "GLUT/glut.h"
+
+#endif //PCH_H
